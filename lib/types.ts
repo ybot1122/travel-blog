a019377a3c69983b59fe3a570ab77d8799f058a0
Toby Liu <ybot1122@gmail.com>
2025-07-06 16:03:30 -0700

    data and types

---
+++ lib/types.ts
@@ -0,0 +1,23 @@
+export interface BlogPost {
+  id: string;
+  title: string;
+  slug: string;
+  excerpt: string;
+  content: string;
+  coverImage: string;
+  tags: string[];
+  location: string;
+  publishedAt: string;
+}
+
+export interface Author {
+  name: string;
+  bio: string;
+  avatar: string;
+  socialLinks: {
+    instagram?: string;
+    twitter?: string;
+    facebook?: string;
+    email?: string;
+  };
+}
