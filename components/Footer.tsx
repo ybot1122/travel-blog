84613f281d1c53717f7e1ebdf34c52973969fb06
Toby Liu <ybot1122@gmail.com>
2025-07-06 16:12:37 -0700

    homepage

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <footer>
+      <p>&copy; 2025 Travel Blog</p>
+    </footer>
+  );
+}
---
+++ components/Navigation.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <nav>
+      <h1>Hello</h1>
+    </nav>
+  );
+}
