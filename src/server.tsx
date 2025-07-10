93033f9105645193ac96577b1794313d2cbafed0
Toby Liu <ybot1122@gmail.com>
2025-07-10 11:07:11 -0700

    initial tailwindcss install

--- src/server.tsx
+++ src/server.tsx
@@ -3,7 +3,16 @@ import { renderToString } from "react-dom/server";
 import App from "./App.tsx";
 
 Deno.serve(() => {
-  const html = `<!DOCTYPE html>${renderToString(<App />)}`;
+  const html = `<!DOCTYPE html>
+  <head>
+     <link rel="stylesheet" href="/style.css">
+</head>
+  
+<body>
+  ${renderToString(<App />)}
+  </body>
+  
+  `;
   return new Response(html, {
     headers: { "Content-Type": "text/html; charset=utf-8" },
   });
