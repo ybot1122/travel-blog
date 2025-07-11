87112ce39b20e65b5a96b6b40270178e50168ec7
Toby Liu <ybot1122@gmail.com>
2025-07-11 10:34:51 -0700

    Search page renders

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-
 export default function () {
   return (
     <nav>
-      <img
-        src={"/static/logo.png"}
-        alt={"My Travel Blog"}
-        width={150}
-        height={50}
-      />
+      <a href="/">
+        <img
+          src={"/static/logo.png"}
+          alt={"My Travel Blog"}
+          width={150}
+          height={50}
+        />
+      </a>
     </nav>
   );
 }
